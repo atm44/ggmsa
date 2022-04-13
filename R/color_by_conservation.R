9aedfaa9f67654a59bc3fbc226d5456a8111511d
Leonel <leonelatm44@gmail.com>
2022-04-13 12:55:01 -0300

    Update color_by_conservation.R

--- R/color_by_conservation.R
+++ R/color_by_conservation.R
@@ -1,6 +1,6 @@
-color_increment <- function(conservation_visibility){
+color_increment <- function(conservation_visibility,bias_cons){
     lapply(1:nrow(conservation_visibility), function(i){
-        color_ramp <- colorRampPalette(colors = c(conservation_visibility[i,"color"], "#ffffff"))
+        color_ramp <- colorRampPalette(colors = c(conservation_visibility[i,"color"], "#ffffff"),bias = bias_cons)
         color_change <- rev(color_ramp(100))[conservation_visibility[i,"visibility"]]
         return(color_change)
         }) %>% unlist #%>% do.call("rbind",.)
@@ -8,7 +8,7 @@ color_increment <- function(conservation_visibility){
 }
 
 
-color_visibility <- function(y){
+color_visibility <- function(y,bias_cons){
     #options(digits = 2)
     #on.exit()
     conser_data <- bar_data(y)
